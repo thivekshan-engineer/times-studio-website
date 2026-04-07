02e6125e617877955ae0ce82e85b91ca5a9b00b5
R.Thivekshan <r.thivekshan@RThivekshans-MacBook-Pro.local>
2026-04-07 11:21:02 +0100

    Fix nodemailer IPv4

--- backend/routes/Bookings.js
+++ backend/routes/Bookings.js
@@ -5,7 +5,10 @@ const nodemailer = require('nodemailer');
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
